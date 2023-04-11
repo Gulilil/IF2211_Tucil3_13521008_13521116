77f840d56f8b66890f26d9faddb57061e33accc5
Jason Rivalino <jasonrivalino@gmail.com>
2023-04-12 03:49:33 +0700

    feat: remove print & time

--- src/backend/solver.go
+++ src/backend/solver.go
@@ -2,43 +2,42 @@ package backend
 
 import (
 	"fmt"
-	"time"
 )
 
 type Solver struct {
 	solRoute Route
-	start int64
-	stop int64
 }
 
 func (s *Solver) SolveUCS(g Graph, startVKey string, endVKey string) {
 
-	s.StartTime()
 	// Declaring Variables
 	q := &QueueRoute{}
 	curRoute := &Route{}
+
 	// Preparing Variables Setup
 	curRoute.InsertLastVertex(*g.GetVertex(startVKey))
 	q.Enqueue(*curRoute)
 
 	count := 0
 	check := false
-	for (!check) {
 
-		if (q.nRoute == 0){
+	// Looping for finding solution
+	for !check {
+
+		if q.nRoute == 0 {
 			break
 		}
 
 		curRoute = q.Dequeue()
 		curVertex := curRoute.GetLastVertex()
 
-		if (IsSolution(*curRoute, endVKey)){
+		if IsSolution(*curRoute, endVKey) {
 			check = true
 			break
-		}		
+		}
 
 		availableEdges := AvailableEdges(*curRoute, g.GetEdgeWithStartV(curVertex))
-		if (len(availableEdges) != 0){ 
+		if len(availableEdges) != 0 {
 			temp := &Route{}
 			temp.CopyConstructorRoute(curRoute)
 			for i, e := range availableEdges {
@@ -51,20 +50,11 @@ func (s *Solver) SolveUCS(g Graph, startVKey string, endVKey string) {
 			}
 		}
 		q.SortAscending()
-
-		// HANYA UNTUK CHECKING
-		// fmt.Println(curVertex)
-		// fmt.Println("=========")
-		// fmt.Print("CURROUTE : ")
-		// curRoute.DisplayRoute()
-		// q.DisplayQueue()
-		// fmt.Println("=========")
-
 		count++
 	}
-	s.StopTime()
 
-	if (!check){
+	// Checking if solution found
+	if !check {
 		fmt.Println("No Solution Found")
 	} else {
 		s.solRoute.CopyConstructorRoute(curRoute)
@@ -73,10 +63,10 @@ func (s *Solver) SolveUCS(g Graph, startVKey string, endVKey string) {
 
 func (s *Solver) SolveAStar(g Graph, startVKey string, endVKey string) {
 
-	s.StartTime()
 	// Declaring Variables
 	q := &QueueRoute{}
 	curRoute := &Route{}
+
 	// Preparing Variables Setup
 	curRoute.InsertLastVertex(*g.GetVertex(startVKey))
 	q.Enqueue(*curRoute)
@@ -85,22 +75,24 @@ func (s *Solver) SolveAStar(g Graph, startVKey string, endVKey string) {
 
 	count := 0
 	check := false
-	for (!check) {
 
-		if (q.nRoute == 0){
+	// Looping for finding solution
+	for !check {
+
+		if q.nRoute == 0 {
 			break
 		}
 
 		curRoute = q.Dequeue()
 		curVertex := curRoute.GetLastVertex()
 
-		if (IsSolution(*curRoute, endVKey)){
+		if IsSolution(*curRoute, endVKey) {
 			check = true
 			break
-		}		
+		}
 
 		availableEdges := AvailableEdges(*curRoute, g.GetEdgeWithStartV(curVertex))
-		if (len(availableEdges) != 0){ 
+		if len(availableEdges) != 0 {
 			temp := &Route{}
 			temp.CopyConstructorRoute(curRoute)
 			for i, e := range availableEdges {
@@ -114,53 +106,31 @@ func (s *Solver) SolveAStar(g Graph, startVKey string, endVKey string) {
 			}
 		}
 		q.SortAStarAscending()
-
-		// HANYA UNTUK CHECKING
-		// fmt.Println(curVertex)
-		// fmt.Println("=========")
-		// fmt.Print("CURROUTE : ")
-		// curRoute.DisplayRoute()
-		// q.DisplayQueue()
-		// fmt.Println("=========")
-
 		count++
 	}
-	s.StopTime()
 
-	if (!check){
+	// Checking if solution found
+	if !check {
 		fmt.Println("No Solution Found")
 	} else {
 		s.solRoute.CopyConstructorRoute(curRoute)
 	}
 }
 
-func AvailableEdges(r Route, edges []*Edge) [] *Edge {
+func AvailableEdges(r Route, edges []*Edge) []*Edge {
 	result := []*Edge{}
 	for _, e := range edges {
-		if (!IsContainVertex(r.buffer, e.endVertex)){
+		if !IsContainVertex(r.buffer, e.endVertex) {
 			result = append(result, e)
 		}
 	}
 	return result
 }
 
-func (s *Solver) StartTime() {
-	s.start = time.Now().UnixNano() / int64(time.Millisecond)
-}
-
-func (s *Solver) StopTime() {
-	s.stop = time.Now().UnixNano() / int64(time.Millisecond)
-}
-
-func (s *Solver) DisplaySolutionRoute(){
+func (s *Solver) DisplaySolutionRoute() {
 	s.solRoute.DisplayRoute()
-}
-
-func (s *Solver) GetExecutionTime() (int64){
-	return s.stop-s.start	
 }
 
 func IsSolution(r Route, endVKey string) bool {
 	return r.GetLastVertex().key == endVKey
 }
-
